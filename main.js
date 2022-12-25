19c4d1a3414cc50451dd757c15ec64cec42dec88
adityamahendrraa <93446838+adityamahendrraa@users.noreply.github.com>
2022-12-25 14:07:29 +0700

    Update main.js
    Signed-off-by: adityamahendrraa <93446838+adityamahendrraa@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,44 +1,20 @@
-$(document).ready(function () {
-
-  all_notes = $("li a");
-
-  all_notes.on("keyup", function () {
-
-    note_title = $(this).find("h2").text();
-
-    note_content = $(this).find("p").text();
-
-    item_key = "list_" + $(this).parent().index();
-
-    data = {
-
-      title: note_title,
-
-      content: note_content
-
-    };
-
-    window.localStorage.setItem(item_key, JSON.stringify(data));
-
-  });
-
-  all_notes.each(function (index) {
-
-    data = JSON.parse(window.localStorage.getItem("list_" + index));
-
-    if (data !== null) {
-
-      note_title = data.title;
-
-      note_content = data.content;
-
-      $(this).find("h2").text(note_title);
-
-      $(this).find("p").text(note_content);
-
-    }
-
-  });
-
-});
-
+$(function(){
+    var overlay = $('<div id="overlay"></div>');
+    overlay.show();
+    overlay.appendTo(document.body);
+    $('.popup-onload').show();
+    $('.close').click(function(){
+    $('.popup-onload').hide();
+    overlay.appendTo(document.body).remove();
+    return false;
+    });
+    
+    
+     
+    
+    $('.x').click(function(){
+    $('.popup').hide();
+    overlay.appendTo(document.body).remove();
+    return false;
+    });
+    });
